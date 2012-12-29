c8ded87856cac10a2df7c667c53315491c868184
Lushi Li <lushili06@gmail.com>
2012-12-29 16:17:20 -0500

    Finish user edit and update actions

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ gem 'rails', '3.2.9'
 gem 'bootstrap-sass', '2.1'
 gem 'table_for_collection'
 gem 'bcrypt-ruby', '3.0.1'
+gem 'faker', '1.0.1'
 
 group :development, :test do
   gem 'sqlite3', '1.3.5'
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,16 +3,16 @@ class SessionsController < ApplicationController
 	def new
 	end
 
-	def create
-		user = User.find_by_email(params[:session][:email].downcase)
+	 def create
+    user = User.find_by_email(params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-			sign_in user
-			redirect_to user    # NEED TO CHANGE. Should redirect to user's task page.
-  	else
-    	flash.now[:error] = 'Invalid email/password combination' 
+      sign_in user
+      redirect_back_or user #NEED TO CHANGE REDIRECT TO TASKLIST
+    else
+      flash.now[:error] = 'Invalid email/password combination'
       render 'new'
-  	end
-	end
+    end
+  end
 
 	def destroy
 		sign_out
